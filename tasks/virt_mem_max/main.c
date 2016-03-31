0611d9f65e606a361936fac1e725c51f04bf8040
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-03-31 17:51:27 +0300

    virt mem max pt 2

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -3,24 +3,28 @@
 
 int main()
 {
-	
+	size_t result = 0;
     	size_t bytes_to_allocate = 1;
 	void* res = NULL;
 	while (res != MAP_FAILED) 
 	{
 		void* addr = mmap(NULL, bytes_to_allocate, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
 		bytes_to_allocate *= 2;
+		result+= bytes_to_allocate;
 		res = addr;
 	}
 
 	size_t bytes_to_allocate1 = bytes_to_allocate / 2;
+	result += bytes_to_allocate1;
+	
 	while (res != MAP_FAILED)
 	{
 		void* addr = mmap(NULL, bytes_to_allocate1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
 		bytes_to_allocate1 /= 2;
+		result += bytes_to_allocate1;
 		res = addr;
 	}
 
-	printf("Allocated: %zu bytes of memory\n", bytes_to_allocate + bytes_to_allocate1);
+	printf("Allocated: %zu bytes of memory\n", result);
 	return 0;
 }
